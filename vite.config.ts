8df8389cfcce3fe440ff28f593c872295e88deab
Pedro Lucas Carolino Felix <pedro.carolino.develop@gmail.com>
2026-06-26 18:24:02 -0300

    config: adicionar workflow do GitHub Actions para o Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Aplicativo-de-Gestao-de-Frota/',
   server: {
     host: true,
     port: 5173,
